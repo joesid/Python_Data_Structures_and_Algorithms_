b69d7e64b46e339c7a20448a358d4ceb7a976556
Joe <joesidious@gmail.com>
2023-05-21 20:31:58 +0100

    Initial Commit

---
+++ DaysBetweenDates_Quiz.py
@@ -0,0 +1,15 @@
+#Quiz
+
+#Write isLeapYear(year)
+#write stub daysInMonth(year, month) that always returns 30
+#modify daysInMonth(year, month) to account for leap years
+#modify nextDay(year, month, day) to use daysInMonth(year, month)
+#test isLeapYear(year)
+#test daysInMonth(year, month) except for leap years
+#test daysInMonth(year, month) including leap years
+#test nextDay(year, month, day) using stub daysInMonth
+#test daysBetweenDates(...) for simple cases
+#test daysBetweenDates(...) for all test cases 
+
+
+
