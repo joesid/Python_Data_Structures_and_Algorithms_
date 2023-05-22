5302db47f976253107b688c52dded7ad4c5452cd
Joe <joesidious@gmail.com>
2023-05-22 20:54:52 +0100

    DaysInMonth

--- DaysBetweenDates_Quiz.py
+++ DaysBetweenDates_Quiz.py
@@ -1,16 +1,18 @@
 #Quiz
 
-#Write isLeapYear(year)
-#write stub daysInMonth(year, month) that always returns 30
-#modify daysInMonth(year, month) to account for leap years
-#modify nextDay(year, month, day) to use daysInMonth(year, month)
-#test isLeapYear(year)
-#test daysInMonth(year, month) except for leap years
-#test daysInMonth(year, month) including leap years
-#test nextDay(year, month, day) using stub daysInMonth
-#test daysBetweenDates(...) for simple cases
-#test daysBetweenDates(...) for all test cases 
+#  A Write isLeapYear(year)
+#  B write stub daysInMonth(year, month) that always returns 30
+#  c modify daysInMonth(year, month) to be correct except for leap years
+#  D modify daysInMonth(year,)to account for leap years
+#  E modify nextDay(year, month, day) to use daysInMonth(year, month)
+#  F test isLeapYear(year)
+#  G test daysInMonth(year, month) except for leap years
+#  H test daysInMonth(year, month) including leap years
+#  I test nextDay(year, month, day) using stub daysInMonth
+#  J test daysBetweenDates(...) for simple cases
+#  K test daysBetweenDates(...) for all test cases 
 
+#BEICIAFK
 
 def isLeapYear(year):
     boolval = False
@@ -21,3 +23,19 @@ def isLeapYear(year):
     return boolval
 
 isLeapYear(2020)
+
+
+def daysInMonth(year, month):
+    if month == 4 or  6 or 9 or 11:
+        return 30
+    elif month == 2:
+        return 28
+    else:
+        return 31
+    
+
+
+
+
+
+
