7f4291b41970d535b94bcafa17a69e67bd537a4e
Joe <joesidious@gmail.com>
2023-05-27 13:29:51 +0100

    Fix dayInMonth_ly returns int

--- DaysBetweenDates_Quiz.py
+++ DaysBetweenDates_Quiz.py
@@ -22,20 +22,27 @@ def isLeapYear(year):
     
 
 def daysInMonth(month):
+    day = 0
     if month == 2:
-        return 28
+        day = 28
     elif month == 4 or month == 6:
-        return 30
+        day = 30
     elif month == 9 or month == 11:
-        return 30
+        day = 30
     else:
-        return 31
+        day = 31
+
+    return day 
 
 def dayInMonth_ly(year,month):
     if month == 2 and isLeapYear(year):
         return 29
-    elif isLeapYear(year) == False:
-        return daysInMonth(month)
+    #elif isLeapYear(year) == False:
+    #   days = int(daysInMonth(month))
+    #   return days 
+    else:
+        days = daysInMonth(month)
+        return days 
 
 def nextDay(year, month, day):
 
@@ -56,34 +63,48 @@ def daysleft(year, month, day):
 
 
 def daysBtwnMonth(year, month2, month1):
-    #cmonth1 =-1
-   
+    #Days Between Month, count the number of dates between months``
+    total_days = 0
+    month1 +=1
     for month in range(month1, month2):
-        days = dayInMonth_ly(year,month)
-        print(type(days))
+        if isLeapYear(year) and month == 2:
+            total_days += 29
+        else:
+            total_days += daysInMonth(month)
+
+    return total_days    
        
-
-
-    pass
 
 def dayBetweenDate(year2, month2, day2, year1, month1, day1):
     day_num = 0
+    dayInMonth1 = dayInMonth_ly(year1, month1)
+    dayInMonth2 = dayInMonth_ly(year2, month2)
+    add = daysBtwnMonth(year1, month2, month1)
     
     if year2 == year1 and month2 == month1:
         day_num = day2 - day1
     elif year2 == year1 and month2 > month1:
-        pass
-
+        day_num = add + (dayInMonth1 - day1) + (dayInMonth2 - (dayInMonth2 - day2))
     
 
+    pass
+
+    
+day = dayInMonth_ly(2021,2)
+print((day))
+
+def test():  
+
+    dates = dayBetweenDate(2020,2,3,2020,1,15)
+    print(dates)
+
+    #days = dayInMonth_ly(2021, 4)
+    #print("Days in function leap year: {}".format(days))
+
+    #count = daysBtwnMonth(2020, 3, 1)
+    #print(count)
 
 
-daysBtwnMonth(2020, 5, 1)
+    #print(daysleft(2, 2020, 28))
 
-
-print(daysleft(2, 2020, 28))
-
-print(nextDay(2020,2,28))
-
-
-
+    #print(nextDay(2020,2,28))
