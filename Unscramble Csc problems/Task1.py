1497b612bf7d6585e13fa53f595de983cc0f3118
joesid <joesidious@gmail.com>
2023-06-26 08:20:39 +0100

    Sort d real 'total" number of phone no in call.csv

--- Unscramble Csc problems/Task1.py
+++ Unscramble Csc problems/Task1.py
@@ -4,6 +4,7 @@ It's ok if you don't understand how to read files.
 """
 import csv
 import re
+import os 
 
 with open('texts.csv', 'r') as f:
     reader = csv.reader(f)
@@ -40,23 +41,51 @@ fix = 0
 mobile = 0
 tele = 0
 
+my_fix=[] 
+my_mobile=[]
+my_tele=[]
+
 for i in my_calls:
    
     if re.search(fixed_pattern,i):
         fix +=1
+        my_fix.append(i)
+        #print(i)
     elif re.search(mobile_pattern, i):
          mobile+=1
+         my_mobile.append(i)
+        
     elif re.search(telemarketer_pattern, i):
          tele+=1
-         
-         
+         my_tele.append(i)
+    
+# for i in my_calls:
+#      print(i)
+
         
-print("there are {} of fixed phone lines".format(fix))
-print("there are {} of mobile phone lines".format(mobile))
-print("there are {} of telemarketer phone lines".format(tele))
 
+total = fix + mobile + tele
+print(total)
 
+unique_mobile = list(set(my_mobile))
+unique_mobile.sort()
 
+unique_fix = list(set(my_fix))
+unique_fix.sort()
+
+unique_tele = list(set(my_tele))
+unique_tele.sort()
+
+with open("fixed_mobile.txt", "w") as file:
+     file.truncate()
+     for item in unique_fix:
+          file.write(str(item) + '\n')
+
+print("there are {} of fixed phone lines".format(len(unique_fix)))
+print("there are {} of mobile phone lines".format(len(unique_mobile)))
+print("there are {} of telemarketer phone lines".format(len(unique_tele)))
+
+    
 """
 TASK 1:
 How many different telephone numbers are there in the records? 
