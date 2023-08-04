efc451c4f149c0d143e73c3cc3018dece8e4c255
joesid <joesidious@gmail.com>
2023-08-04 08:18:15 +0100

    Task 4 Completed

---
+++ Unscramble Csc problems/Task4.py
@@ -0,0 +1,60 @@
+"""
+Read file into texts and calls.
+It's ok if you don't understand how to read files.
+"""
+import csv
+import re
+
+with open('texts.csv', 'r') as f:
+    reader = csv.reader(f)
+    texts = list(reader)
+
+with open('calls.csv', 'r') as f:
+    reader = csv.reader(f)
+    calls = list(reader)
+
+send_texts = []
+mobile_pattern = r'[789]\d{3,5}\s\d+'
+callers_list = []
+mobile_callers = []
+
+send_texts = {row[0] for row in texts}
+    
+# for log in calls:
+#     if re.match(mobile_pattern, log[0]):
+#         mobile_callers.append(log[0])
+        
+mobile_callers = {row[0] for row in calls if re.match(mobile_pattern, row[0])}
+
+
+matched_no  = []
+
+def intersect(a, b):
+    intersect_set = set()
+    for element in a:
+        if element in b:
+            intersect_set.add(element)
+    return intersect_set
+
+matched_no = intersect(send_texts, mobile_callers)
+
+list_tele = list(set(matched_no))
+
+print("These numbers could be telemarketers: ")
+
+for i in list_tele:
+    print(i)
+
+"""
+TASK 4:
+The telephone company want to identify numbers that might be doing
+telephone marketing. Create a set of possible telemarketers:
+these are numbers that make outgoing calls but never send texts,
+receive texts or receive incoming calls.
+
+Print a message:
+"These numbers could be telemarketers: "
+<list of numbers>
+The list of numbers should be print out one per line in lexicographic order with no duplicates.
+"""
+
